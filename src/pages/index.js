4170f83919659a9d5a2fcf33e8cd0b35881bc108
Ozbey <unrealty@hotmail.com>
2017-10-01 14:04:20 +0200

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>CardioChain</h1>
+    <p>Official website of CardioChain.</p>
+    <p>Coming soon...</p>
+  </div>
+)
+
+export default IndexPage
